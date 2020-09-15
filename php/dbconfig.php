39ab810edcf2a094b9842c15a55a4508f92023d4
Kioselsa <osel0810029@gmail.com>
2020-09-14 22:24:23 -0500

    Creacion de archivo gitignore

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -12,12 +12,12 @@
     define("DB_NAME2", "caracterizacion");
 
     //Sql Server
-    //define("MS_DB_SERVER", "172.16.0.10");
-    define("MS_DB_SERVER", "localhost"); //Para pruebas
-    //define("MS_DB_USER", "sista");
-    define("MS_DB_USER", "sa");  //Para pruebas
-    //define("MS_DB_PASS", "Sta_2019$");
-    define("MS_DB_PASS", "Adminooapas!"); //Para pruebas
+    define("MS_DB_SERVER", "172.16.0.10");
+    //define("MS_DB_SERVER", "localhost"); //Para pruebas
+    define("MS_DB_USER", "sista");
+    //define("MS_DB_USER", "sa");  //Para pruebas
+    define("MS_DB_PASS", "Sta_2019$");
+    //define("MS_DB_PASS", "Adminooapas!"); //Para pruebas
     define("MS_DB_NAME", "CONTROL_ESCOLAR");
 
 ?>
