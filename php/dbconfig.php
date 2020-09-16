fc9b4785ed46071aebd717beb8bcb7e27c77e599
Jorge Armando Rocha Mendoza <isc.armando.rocha@gmail.com>
2020-09-16 15:49:37 -0500

    Correcciones 16-09-2020 merge

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -18,11 +18,7 @@
     define("MS_DB_USER", "sista");
     //define("MS_DB_USER", "sa");  //Para pruebas
     define("MS_DB_PASS", "Sta_2019$");
-<<<<<<< HEAD
-    //define("MS_DB_PASS", "Adminooapas!"); //Para pruebas
-=======
     //define("MS_DB_PASS", "12345"); //Para pruebas
->>>>>>> 56411be3d4f2143003430641832cd7de4d76ec99
     define("MS_DB_NAME", "CONTROL_ESCOLAR");
 
 ?>
